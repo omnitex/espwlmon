ae75bc40d9a99313bc91375452682798d1cd88dc
omnitex <omnitex.git@gmail.com>
2023-02-20 09:31:34 +0100

    Update function signature for linux target

--- data-collector/components/wlmon/wlmon_linux.cpp
+++ data-collector/components/wlmon/wlmon_linux.cpp
@@ -3,17 +3,17 @@
 
 static const char *TAG = "wlmon";
 
-const esp_partition_t *get_wl_partition(void *arg)
+esp_err_t get_wl_partition(void *arg, const esp_partition_t **partition)
 {
     ESP_LOGD(TAG, "linux get_wl_partition(%s)", arg);
     // TODO get the partition dump file, copy to allocated memory (?)
     // and allocate esp_partition_t and fill with needed values, return it
-    return (esp_partition_t *)NULL;
+    return ESP_ERR_NOT_SUPPORTED;
 }
 
 esp_err_t get_wl_config(wl_config_t *cfg, const esp_partition_t *part)
 {
     ESP_LOGD(TAG, "linux get_wl_config()");
 
-    return ESP_OK;
+    return ESP_ERR_NOT_SUPPORTED;
 }
