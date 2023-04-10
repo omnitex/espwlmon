f688d9c0f4a537ca88b039eebbf5ae39ce40064d
omnitex <omnitex.git@gmail.com>
2023-04-10 11:07:04 +0200

    Heatmap from data with matplotlib helper functions

--- espwlmon.py
+++ espwlmon.py
@@ -6,13 +6,15 @@ import argparse
 import sys
 import json
 import serial
+import math
 
 import PySimpleGUI as sg
-#import matplotlib
+import matplotlib
 import matplotlib.pyplot as plt
 
 import plotly.express as px
 import plotly.tools as tls
+import plotly.io as pio
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
@@ -58,11 +60,136 @@ def monitor(port):
 
     gui(json_dict)
 
+# source https://matplotlib.org/stable/gallery/images_contours_and_fields/image_annotated_heatmap.html
+def create_heatmap(data, row_labels, col_labels, ax=None,
+            cbar_kw=None, cbarlabel="", **kwargs):
+    """
+    Create a heatmap from a numpy array and two lists of labels.
+
+    Parameters
+    ----------
+    data
+        A 2D numpy array of shape (M, N).
+    row_labels
+        A list or array of length M with the labels for the rows.
+    col_labels
+        A list or array of length N with the labels for the columns.
+    ax
+        A `matplotlib.axes.Axes` instance to which the heatmap is plotted.  If
+        not provided, use current axes or create a new one.  Optional.
+    cbar_kw
+        A dictionary with arguments to `matplotlib.Figure.colorbar`.  Optional.
+    cbarlabel
+        The label for the colorbar.  Optional.
+    **kwargs
+        All other arguments are forwarded to `imshow`.
+    """
+
+    if ax is None:
+        ax = plt.gca()
+
+    if cbar_kw is None:
+        cbar_kw = {}
+
+    # Plot the heatmap
+    im = ax.imshow(data, **kwargs)
+
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+
+    # Show all ticks and label them with the respective list entries.
+    ax.set_xticks(np.arange(data.shape[1]), labels=col_labels)
+    ax.set_yticks(np.arange(data.shape[0]), labels=row_labels)
+
+    # Let the horizontal axes labeling appear on top.
+    ax.tick_params(top=True, bottom=False,
+                   labeltop=True, labelbottom=False)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=-30, ha="right",
+             rotation_mode="anchor")
+
+    # Turn spines off and create white grid.
+    ax.spines[:].set_visible(False)
+
+    ax.set_xticks(np.arange(data.shape[1]+1)-.5, minor=True)
+    ax.set_yticks(np.arange(data.shape[0]+1)-.5, minor=True)
+    ax.grid(which="minor", color="w", linestyle='-', linewidth=0)
+    ax.tick_params(which="minor", bottom=False, left=False)
+
+    return im, cbar
+
+
+def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
+                     textcolors=("black", "white"),
+                     threshold=None, **textkw):
+    """
+    A function to annotate a heatmap.
+
+    Parameters
+    ----------
+    im
+        The AxesImage to be labeled.
+    data
+        Data used to annotate.  If None, the image's data is used.  Optional.
+    valfmt
+        The format of the annotations inside the heatmap.  This should either
+        use the string format method, e.g. "$ {x:.2f}", or be a
+        `matplotlib.ticker.Formatter`.  Optional.
+    textcolors
+        A pair of colors.  The first is used for values below a threshold,
+        the second for those above.  Optional.
+    threshold
+        Value in data units according to which the colors from textcolors are
+        applied.  If None (the default) uses the middle of the colormap as
+        separation.  Optional.
+    **kwargs
+        All other arguments are forwarded to each call to `text` used to create
+        the text labels.
+    """
+
+    if not isinstance(data, (list, np.ndarray)):
+        data = im.get_array()
+
+    # Normalize the threshold to the images color range.
+    if threshold is not None:
+        threshold = im.norm(threshold)
+    else:
+        threshold = im.norm(data.max())/2.
+
+    # Set default alignment to center, but allow it to be
+    # overwritten by textkw.
+    kw = dict(horizontalalignment="center",
+              verticalalignment="center")
+    kw.update(textkw)
+
+    # Get the formatter in case a string is supplied
+    if isinstance(valfmt, str):
+        valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
+
+    # Loop over the data and create a `Text` for each "pixel".
+    # Change the text's color depending on the data.
+    texts = []
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            kw.update(color=textcolors[int(im.norm(data[i, j]) > threshold)])
+            text = im.axes.text(j, i, valfmt(data[i, j], None), **kw)
+            texts.append(text)
+
+    return texts
+
 def draw_figure(canvas, figure):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
     figure_canvas_agg.draw()
-    figure_canvas_agg.get_tk_widget().pack(side="top", fill="both", expand=1)
+    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=True, anchor='center')
     return figure_canvas_agg
+
+def format_thousands(num, _):
+    if num >= 1000:
+        return '{:.0f}K'.format(num / 1000)
+    else:
+        return str(num)
 
 #TODO launch GUI before getting JSON with a loading screen
 def gui(json_dict):
@@ -78,6 +205,29 @@ def gui(json_dict):
     sector_count = int(state['max_pos'], base=16) - 1
     print(f'sector_count: {sector_count}')
 
+    sector_count = 257;
+
+    # get heatmap side lengths for given sector_count
+    X = 1
+    Y = sector_count
+    while X < Y:
+        X += 1
+        Y = math.ceil(sector_count / X)
+
+    print(f'X = {X}, Y = {Y}')
+
+    # 2D heatmap to contain all sector counts, init all zeros
+    heatmap = np.zeros((X, Y) ,dtype=int)
+
+    # fill heatmap with values from erase_counts JSON
+    # index with sector num but in 2D
+    for sector_num_str, erase_count_str in erase_counts.items():
+        sector_num = int(sector_num_str)
+        erase_count = int(erase_count_str)
+        # every erase count means 16 erases, as that is the threshold for triggering
+        # writing a record to flash
+        heatmap[sector_num % X][sector_num // X] = 16 * erase_count
+
     left = [[sg.T(f'wl_mode: {wl_mode}')]]
     for key in config:
         left += [[sg.T(f'{key}: {config[key]}')]]
@@ -90,19 +240,26 @@ def gui(json_dict):
 
     window = sg.Window(f'espwlmon v{__version__}', layout, finalize=True, margins=(10,10))
 
-    #TODO placeholder heatmap
-    fig = plt.figure()
-    a = np.random.random((16, 16))
-    plt.imshow(a, cmap='hot', interpolation='nearest')
+    # plot the heatmap
+    fig, ax = plt.subplots()
+    im, _ = create_heatmap(heatmap, np.arange(X), np.arange(Y), ax=ax, cmap='viridis', cbarlabel='erase count')
+
+    annotate_heatmap(im, valfmt=format_thousands, size=8, textcolors=('white', 'black'))
+    fig.tight_layout()
 
     draw_figure(window['-CANVAS-'].TKCanvas, fig)
+
+    # TODO plotly export as html, button?
+    # df = px.data.tips()
+
+    # fig = px.density_heatmap(df, x="total_bill", y="tip", text_auto=True)
+    # fig.write_html('heatmap.html')
 
     while True:
         event, values = window.read()
         if event in (sg.WIN_CLOSED, 'Exit'):
             break
     window.close()
-
 
 def main():
     """
